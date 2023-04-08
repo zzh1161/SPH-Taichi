bedfc163b43d780a4add5a6be709e70b3618b2a3
zhang-zh <1563833973@qq.com>
2023-04-08 21:08:26 +0800

    add video

--- demo_wcsph_2d.py
+++ demo_wcsph_2d.py
@@ -1,12 +1,14 @@
+import os
 import taichi as ti
 import numpy as np
 from particle_system import ParticleSystem
 from wcsph import WCSPHSolver
 
-# ti.init(arch=ti.cpu)
+ti.init(arch=ti.cpu)
+os.chdir(os.path.dirname(__file__))
 
 # Use GPU for higher peformance if available
-ti.init(arch=ti.gpu, device_memory_GB=4)
+# ti.init(arch=ti.gpu, device_memory_GB=4)
 # ti.init(arch=ti.gpu, device_memory_GB=3, packed=True)
 
 
@@ -29,11 +31,16 @@ if __name__ == "__main__":
 
     wcsph_solver = WCSPHSolver(ps)
     gui = ti.GUI(background_color=0x112f41)
+    cnt = 0
     while gui.running:
+        cnt += 1
         for i in range(5):
             wcsph_solver.step()
         particle_info = ps.dump()
         gui.circles(particle_info['position'] * ps.screen_to_world_ratio / 512,
                     radius=ps.particle_radius / 1.5 * ps.screen_to_world_ratio,
                     color=0x068587)
-        gui.show()
+        filename = f'frame/{cnt:04d}.png'
+        print(f'Frame {cnt} is recorded in {filename}')
+        gui.show(filename)
+        # gui.show()
